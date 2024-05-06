f6665e5639eb51f45b83a6775cbb0d3342014265
nguyencnguyen-playon <131789853+nguyencnguyen-playon@users.noreply.github.com>
2024-05-07 01:23:42 +0700

    Update tursoClient.ts

--- libs/tursoClient.ts
+++ libs/tursoClient.ts
@@ -2,7 +2,7 @@ import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 
 const client = createClient({
-    url: process.env.TURSO_DATABASE_URL || '',
+    url: process.env.TURSO_DATABASE_URL || 'libsql://nextjs-pa-db-nguyencnguyen-playon.turso.io',
     authToken: process.env.TURSO_AUTH_TOKEN,
 });
 
